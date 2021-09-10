4923bec7087db49515ca6dd45600b46c8f5211d9
Lloyd Sibson <lloydsibson@gmail.com>
2021-09-11 00:02:55 +0100

    Mobile nav update

--- src/components/mobile-nav.jsx
+++ src/components/mobile-nav.jsx
@@ -14,9 +14,18 @@ import { faBars } from "@fortawesome/free-solid-svg-icons"
 const MobileNav = () => {
   const body = document.body
   const NavItemToggle = () => {
-    body.classList.add("menu-open")
+    body.classList.remove("menu-open")
     body.scrollTop = 0 // For Safari
     document.documentElement.scrollTop = 0 // For Chrome, Firefox, IE and Opera
+    // REMOVES MOBILE NAV AFTER NAV CLOSES
+    setTimeout(() => {
+      const menu = document.querySelector(".mobile-sidebar")
+      menu.style.display = "none"
+    }, 400)
+  }
+
+  const NavBurgerToggle = () => {
+    body.classList.add("menu-open")
     // REMOVES MOBILE NAV AFTER NAV CLOSES
     const menu = document.querySelector(".mobile-sidebar")
     menu.style.display = "flex"
@@ -25,33 +34,41 @@ const MobileNav = () => {
   return (
     <div className="mobile-nav">
       <div className="mobile-nav_inner-container">
-        <Link to="/" activeClassName="active">
+        <Link to="/" activeClassName="active" onClick={e => NavItemToggle(e)}>
           <div className="mobile-nav-icon">
             <FontAwesomeIcon icon={faHome} alt="home icon" />
             {/* <p>Home</p> */}
           </div>
         </Link>
 
-        <Link to="/events" activeClassName="active">
+        <Link
+          to="/events"
+          activeClassName="active"
+          onClick={e => NavItemToggle(e)}
+        >
           <div className="mobile-nav-icon">
             <FontAwesomeIcon icon={faCalendarAlt} alt="events icon" />
             {/* <p>Events</p> */}
           </div>
         </Link>
 
-        <div className="mobile-nav-icon store">
+        <div className="mobile-nav-icon store" onClick={e => NavItemToggle(e)}>
           <FontAwesomeIcon icon={faStore} alt="store icon" />
           {/* <p>Store</p> */}
         </div>
 
-        <Link to="/contact" activeClassName="active">
+        <Link
+          to="/contact"
+          activeClassName="active"
+          onClick={e => NavItemToggle(e)}
+        >
           <div className="mobile-nav-icon">
             <FontAwesomeIcon icon={faComment} alt="comment icon" />
             {/* <p>Contact</p> */}
           </div>
         </Link>
 
-        <div className="mobile-nav-icon menu" onClick={e => NavItemToggle(e)}>
+        <div className="mobile-nav-icon menu" onClick={e => NavBurgerToggle(e)}>
           <FontAwesomeIcon icon={faBars} alt="menu icon" />
           {/* <p>Menu</p> */}
         </div>
--- src/components/mobile-sidebar.jsx
+++ src/components/mobile-sidebar.jsx
@@ -4,11 +4,26 @@ import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 import "./mobile-sidebar.scss"
 
+// FONT AWESOME ICON
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faStore } from "@fortawesome/free-solid-svg-icons"
+
 const MobileSideBar = () => {
+  const body = document.body
+  const NavItemToggle = () => {
+    body.classList.remove("menu-open")
+    body.scrollTop = 0 // For Safari
+    document.documentElement.scrollTop = 0 // For Chrome, Firefox, IE and Opera
+    // REMOVES MOBILE NAV AFTER NAV CLOSES
+    setTimeout(() => {
+      const menu = document.querySelector(".mobile-sidebar")
+      menu.style.display = "none"
+    }, 400)
+  }
   return (
     <nav className="mobile-sidebar" aria-label="Mobile navigation">
       <div className="mobile-sidebar__logo">
-        <Link to="/">
+        <Link to="/" onClick={e => NavItemToggle(e)}>
           <StaticImage
             src="../images/website-logo.png"
             width={100}
@@ -20,7 +35,7 @@ const MobileSideBar = () => {
       </div>
       <ul className="nav-list section-one">
         <li>
-          <Link to="/" activeClassName="active">
+          <Link to="/" activeClassName="active" onClick={e => NavItemToggle(e)}>
             Home
           </Link>
         </li>
@@ -30,27 +45,47 @@ const MobileSideBar = () => {
           </Link>
         </li>
         <li>
-          <Link to="/team" activeClassName="active">
+          <Link
+            to="/team"
+            activeClassName="active"
+            onClick={e => NavItemToggle(e)}
+          >
             Team
           </Link>
         </li>
         <li>
-          <Link to="/partners" activeClassName="active">
+          <Link
+            to="/partners"
+            activeClassName="active"
+            onClick={e => NavItemToggle(e)}
+          >
             Partners
           </Link>
         </li>
         <li>
-          <Link to="/artists" activeClassName="active">
+          <Link
+            to="/artists"
+            activeClassName="active"
+            onClick={e => NavItemToggle(e)}
+          >
             Artists
           </Link>
         </li>
         <li>
-          <Link to="/arclight-promotions" activeClassName="active">
+          <Link
+            to="/arclight-promotions"
+            activeClassName="active"
+            onClick={e => NavItemToggle(e)}
+          >
             Arclight Promotions
           </Link>
         </li>
         <li>
-          <Link to="/arclight-apparel" activeClassName="active">
+          <Link
+            to="/arclight-apparel"
+            activeClassName="active"
+            onClick={e => NavItemToggle(e)}
+          >
             Arclight Apparel
           </Link>
         </li>
@@ -58,21 +93,46 @@ const MobileSideBar = () => {
       <div className="nav-divider" role="presentation"></div>
       <ul className="nav-list section-two">
         <li>
-          <Link to="/cookie-policy" activeClassName="active">
+          <Link
+            to="/cookie-policy"
+            activeClassName="active"
+            onClick={e => NavItemToggle(e)}
+          >
             Cookies
           </Link>
         </li>
         <li>
-          <Link to="/privacy-policy" activeClassName="active">
+          <Link
+            to="/privacy-policy"
+            activeClassName="active"
+            onClick={e => NavItemToggle(e)}
+          >
             Privacy
           </Link>
         </li>
         <li>
-          <Link to="/returns-and-refunds" activeClassName="active">
+          <Link
+            to="/returns-and-refunds"
+            activeClassName="active"
+            onClick={e => NavItemToggle(e)}
+          >
             Returns & Refunds
           </Link>
         </li>
       </ul>
+
+      <div className="mobile-nav-cta">
+        <div className="mobile-nav-cta__inner-container">
+          <div className="mobile-nav-cta-icon">
+            <FontAwesomeIcon icon={faStore} alt="mobile nav store icon" />
+          </div>
+          <p className="mobile-nav-cta-title">Store Coming Soon</p>
+          <p className="mobile-nav-cta-text">
+            Limited Editions runs and collaboration merchandise you won’t find
+            anywhere else
+          </p>
+        </div>
+      </div>
       {/* <div class="mobile-sidebar__footer">
         <Link to="/members" activeClassName="active">
           Members
