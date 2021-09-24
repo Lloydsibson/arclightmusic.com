4df0b6111da9939c5d613ccb2a003d7887b5c8ed
Lloyd Sibson <lloydsibson@gmail.com>
2021-09-24 12:44:23 +0100

    Event page update

--- src/components/api/events-eventbrite-api.jsx
+++ src/components/api/events-eventbrite-api.jsx
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from "react"
 // SASS
 //import "./eventbrite-api.scss"
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-// import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons"
 // import { faCalendarAlt } from "@fortawesome/free-solid-svg-icons"
 
 const EventPageEventbriteAPI = () => {
-  const [numberOfCurrentEvents, setNumberOfCurrentEvents] = useState("")
+  const [numberOfCurrentEvents, setNumberOfCurrentEvents] = useState()
   const [eventBriteAPIData, setEventBriteAPIData] = useState([])
 
   console.log(eventBriteAPIData)
+
   useEffect(() => {
     const fetchEventApi = async () => {
       try {
@@ -127,6 +128,27 @@ const EventPageEventbriteAPI = () => {
     fetchEventApi()
   }, [])
 
+  // SHARE BUTTON FUNCTIONALITY
+  const shareButtonHandler = e => {
+    const cardURL = e.currentTarget.nextSibling.lastChild.getAttribute("href")
+    if (navigator.share) {
+      navigator
+        .share({
+          title: "Arclight Music - Upcoming Event",
+          url: `${cardURL}`,
+        })
+        .then(() => {
+          // DO NOTHING
+        })
+        .catch(console.error)
+    } else {
+      // fallback
+      alert(
+        "Your Current Browser Doesn't Support Sharing. Please Try Another Browser"
+      )
+    }
+  }
+
   return (
     <div className="events-page">
       <div className="events-page__inner-container">
@@ -134,7 +156,7 @@ const EventPageEventbriteAPI = () => {
           <div className="events-title-container__title">
             <h1>Events</h1>
             <div id="events-counter">
-              {numberOfCurrentEvents > 0 ? numberOfCurrentEvents : "0"}
+              {numberOfCurrentEvents > 0 ? numberOfCurrentEvents : 0}
             </div>
           </div>
           <div className="events-title-container__filters">
@@ -150,6 +172,73 @@ const EventPageEventbriteAPI = () => {
               <h3>Past Events</h3>
             </a>
           </div>
+        </div>
+        <div className="eventpage-card-container">
+          {eventBriteAPIData.map((data, key) => (
+            <div className="event-card" key={key}>
+              <div className="event-card__poster">
+                <img src={data.eventImage} alt="event poster" />
+              </div>
+              <div className="event-card__information">
+                <h2>{data.eventTitle}</h2>
+                <div className="event-tag-container">
+                  <div className="event-tag-container__venue">
+                    <div className="venue-map-icon">
+                      <div className="venue-map-icon__map">
+                        <FontAwesomeIcon icon={faMapMarkerAlt} />
+                      </div>
+                    </div>
+                    <h4>{data.eventVenueName}</h4>
+                  </div>
+                  <div className="event-tag-container__category">
+                    <h4>{data.eventSubCategory}</h4>
+                  </div>
+                </div>
+
+                <p className="event-summary">{data.eventSummary}</p>
+                <div className="event-time-container">
+                  <p>Start Time</p>
+                  <h5>
+                    {data.eventDay} {data.eventMonth}, {data.eventTime}
+                  </h5>
+                </div>
+
+                <div className="event-card-footer-container">
+                  <div
+                    className="event-card-footer-container__share"
+                    onClick={e => shareButtonHandler(e)}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="43"
+                      height="43"
+                      viewBox="0 0 43 43"
+                      fill="none"
+                    >
+                      <circle
+                        cx="21.5"
+                        cy="21.5"
+                        r="20.5"
+                        stroke="#D1D1D1"
+                        strokeWidth="2"
+                      />
+                      <path
+                        d="M26.5 25C25.4403 25 24.4662 25.3665 23.6973 25.9795L18.8933 22.9769C19.0356 22.3334 19.0356 21.6666 18.8933 21.023L23.6973 18.0205C24.4662 18.6335 25.4403 19 26.5 19C28.9853 19 31 16.9853 31 14.5C31 12.0147 28.9853 10 26.5 10C24.0147 10 22 12.0147 22 14.5C22 14.8355 22.037 15.1623 22.1067 15.4769L17.3027 18.4795C16.5338 17.8665 15.5597 17.5 14.5 17.5C12.0147 17.5 10 19.5147 10 22C10 24.4853 12.0147 26.5 14.5 26.5C15.5597 26.5 16.5338 26.1335 17.3027 25.5205L22.1067 28.5231C22.0357 28.8439 21.9999 29.1714 22 29.5C22 31.9853 24.0147 34 26.5 34C28.9853 34 31 31.9853 31 29.5C31 27.0147 28.9853 25 26.5 25Z"
+                        fill="#D1D1D1"
+                      />
+                    </svg>
+                    <p>Share</p>
+                  </div>
+                  <div className="event-card-footer-container__cta">
+                    <h5>{data.eventTicketPrice}</h5>
+                    <a href={data.eventURL}>
+                      <button>Buy Tickets</button>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
