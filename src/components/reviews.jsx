3a268a2eaaf502400b19995829210ca0cf89f202
Lloyd <lloydsibson2021@gmail.com>
2022-01-14 16:31:04 +0000

    Reviews added to homepage

--- src/components/reviews.jsx
+++ src/components/reviews.jsx
@@ -47,7 +47,50 @@ const Reviews = () => {
           <path d="M11.0546 1.47355L12.3428 0.355164C12.8883 -0.118388 13.7703 -0.118388 14.31 0.355164L25.5909 10.1436C26.1364 10.6171 26.1364 11.3829 25.5909 11.8514L14.31 21.6448C13.7645 22.1184 12.8825 22.1184 12.3428 21.6448L11.0546 20.5264C10.5033 20.0479 10.5149 19.267 11.0778 18.7985L18.0703 13.0151H1.3927C0.620913 13.0151 0 12.4761 0 11.806V10.194C0 9.52393 0.620913 8.98489 1.3927 8.98489H18.0703L11.0778 3.20151C10.5091 2.733 10.4975 1.95214 11.0546 1.47355Z"></path>
         </svg>
       </div>
-      <div className="review-message"><p>Enjoyed an event? Leave us a review and share your experience with others &#128515;</p></div>
+      <div className="reviews-container">
+        <div className="review">
+          <img
+            src="https://cdn.trustpilot.net/brand-assets/4.1.0/stars/stars-5.svg"
+            alt="trustpilot score"
+          />
+          <h4>5/5!</h4>
+          <p>
+            The staff are super friendly and great guys all around, and the gigs
+            they put on are amazing!! Great vibes and their merch is high
+            quality. I've never had any problems with the sizing and the price
+            is very fairly priced with fast shipping.
+          </p>
+        </div>
+        <div className="review">
+          <img
+            src="https://cdn.trustpilot.net/brand-assets/4.1.0/stars/stars-5.svg"
+            alt="trustpilot score"
+          />
+          <h4>Great night at the shed seeing the 3…</h4>
+          <p>
+            Great night at the shed seeing the 3 bands organised by Arclight.
+            Good music, good atmosphere and good price!
+          </p>
+        </div>
+        {/* <div className="review">
+          <img
+            src="https://cdn.trustpilot.net/brand-assets/4.1.0/stars/stars-5.svg"
+            alt="trustpilot score"
+          />
+          <h4>Definitely a five star rating!</h4>
+          <p>
+            The tees arrived quickly and are top quality. They are super comfy
+            and the sizing was is perfect! I will definitely be ordering more!
+            Keep up the good work guys! ????
+          </p>
+        </div> */}
+      </div>
+      <div className="review-message">
+        <p>
+          Enjoyed an event? Leave us a review and share your experience with
+          others &#128515;
+        </p>
+      </div>
       <div className="reviews__inner-container">
         {/* <div className="review-names-container">
           {reviewData.map((info, key) => (
@@ -68,7 +111,7 @@ const Reviews = () => {
             <p>"Sick team to work with. 5/5"</p>
           </div>
         </div> */}
-         <TrustPilotReviewCollector />
+        <TrustPilotReviewCollector />
       </div>
     </div>
   )
