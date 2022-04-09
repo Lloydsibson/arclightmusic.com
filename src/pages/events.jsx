95477df457fbb6591b603ebfdc9c77bc6779adac
Lloyd <lloydsibson@gmail.com>
2022-04-09 19:41:06 +0100

    Fade in for event cards on events page

--- src/pages/events.jsx
+++ src/pages/events.jsx
@@ -174,95 +174,103 @@ const EventsPage = () => {
                 >
                   {value[2].loadingMessage}
                 </p>
-                {value[0].eventBriteState.map((data, key) => (
-                  <article
-                    className="event-card"
-                    key={key}
-                    data-category={data.eventSubCategory}
-                  >
-                    <div className="event-card__poster">
-                      {/* <img src={data.eventImage} alt="event poster" /> */}
-                      <div
-                        className="event-img"
-                        style={{ backgroundImage: `url(${data.eventImage})` }}
-                        role="img"
-                      ></div>
-                    </div>
-                    <div className="event-card__information">
-                      <h2>{data.eventTitle}</h2>
-                      <div className="event-tag-container">
-                        <div className="event-tag-container__venue">
-                          <div className="venue-map-icon">
-                            <div className="venue-map-icon__map">
-                              <FontAwesomeIcon icon={faMapMarkerAlt} />
+                <div
+                  className={`eventpage-card-container__events ${
+                    value[3].apiLoaded ? "loaded" : ""
+                  }`}
+                >
+                  {value[0].eventBriteState.map((data, key) => (
+                    <article
+                      className="event-card"
+                      key={key}
+                      data-category={data.eventSubCategory}
+                    >
+                      <div className="event-card__poster">
+                        {/* <img src={data.eventImage} alt="event poster" /> */}
+                        <div
+                          className="event-img"
+                          style={{ backgroundImage: `url(${data.eventImage})` }}
+                          role="img"
+                        ></div>
+                      </div>
+                      <div className="event-card__information">
+                        <h2>{data.eventTitle}</h2>
+                        <div className="event-tag-container">
+                          <div className="event-tag-container__venue">
+                            <div className="venue-map-icon">
+                              <div className="venue-map-icon__map">
+                                <FontAwesomeIcon icon={faMapMarkerAlt} />
+                              </div>
+                            </div>
+                            <h3>{data.eventVenueName}</h3>
+                          </div>
+                          <div className="event-tag-container__category">
+                            <h3>{data.eventSubCategory}</h3>
+                          </div>
+                        </div>
+
+                        <p className="event-summary">{data.eventSummary}</p>
+                        <div className="event-time-container">
+                          <p>Start Time</p>
+                          <h4>
+                            {data.eventDay} {data.eventMonth}, {data.eventTime}
+                          </h4>
+                        </div>
+
+                        <div className="event-card-footer-container">
+                          <div className="event-card-footer-container__inner-container">
+                            <div
+                              className="event-card-footer-container__share"
+                              onClick={e =>
+                                shareButtonHandler(e, data.eventURL)
+                              }
+                            >
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="43"
+                                height="43"
+                                viewBox="0 0 43 43"
+                                fill="none"
+                              >
+                                <circle
+                                  cx="21.5"
+                                  cy="21.5"
+                                  r="20.5"
+                                  stroke="#D1D1D1"
+                                  strokeWidth="2"
+                                />
+                                <path
+                                  d="M26.5 25C25.4403 25 24.4662 25.3665 23.6973 25.9795L18.8933 22.9769C19.0356 22.3334 19.0356 21.6666 18.8933 21.023L23.6973 18.0205C24.4662 18.6335 25.4403 19 26.5 19C28.9853 19 31 16.9853 31 14.5C31 12.0147 28.9853 10 26.5 10C24.0147 10 22 12.0147 22 14.5C22 14.8355 22.037 15.1623 22.1067 15.4769L17.3027 18.4795C16.5338 17.8665 15.5597 17.5 14.5 17.5C12.0147 17.5 10 19.5147 10 22C10 24.4853 12.0147 26.5 14.5 26.5C15.5597 26.5 16.5338 26.1335 17.3027 25.5205L22.1067 28.5231C22.0357 28.8439 21.9999 29.1714 22 29.5C22 31.9853 24.0147 34 26.5 34C28.9853 34 31 31.9853 31 29.5C31 27.0147 28.9853 25 26.5 25Z"
+                                  fill="#D1D1D1"
+                                />
+                              </svg>
+                              <p>Share</p>
+                            </div>
+                            <div className="event-card-footer-container__price">
+                              <h4>{data.eventTicketPrice}</h4>
                             </div>
                           </div>
-                          <h3>{data.eventVenueName}</h3>
-                        </div>
-                        <div className="event-tag-container__category">
-                          <h3>{data.eventSubCategory}</h3>
-                        </div>
-                      </div>
-
-                      <p className="event-summary">{data.eventSummary}</p>
-                      <div className="event-time-container">
-                        <p>Start Time</p>
-                        <h4>
-                          {data.eventDay} {data.eventMonth}, {data.eventTime}
-                        </h4>
-                      </div>
-
-                      <div className="event-card-footer-container">
-                        <div className="event-card-footer-container__inner-container">
-                          <div
-                            className="event-card-footer-container__share"
-                            onClick={e => shareButtonHandler(e, data.eventURL)}
-                          >
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              width="43"
-                              height="43"
-                              viewBox="0 0 43 43"
-                              fill="none"
+                          <div className="event-card-footer-container__btn">
+                            <button
+                              onClick={e =>
+                                eventButtonHandler(
+                                  e,
+                                  data.eventURL,
+                                  data.eventTitle
+                                )
+                              }
                             >
-                              <circle
-                                cx="21.5"
-                                cy="21.5"
-                                r="20.5"
-                                stroke="#D1D1D1"
-                                strokeWidth="2"
-                              />
-                              <path
-                                d="M26.5 25C25.4403 25 24.4662 25.3665 23.6973 25.9795L18.8933 22.9769C19.0356 22.3334 19.0356 21.6666 18.8933 21.023L23.6973 18.0205C24.4662 18.6335 25.4403 19 26.5 19C28.9853 19 31 16.9853 31 14.5C31 12.0147 28.9853 10 26.5 10C24.0147 10 22 12.0147 22 14.5C22 14.8355 22.037 15.1623 22.1067 15.4769L17.3027 18.4795C16.5338 17.8665 15.5597 17.5 14.5 17.5C12.0147 17.5 10 19.5147 10 22C10 24.4853 12.0147 26.5 14.5 26.5C15.5597 26.5 16.5338 26.1335 17.3027 25.5205L22.1067 28.5231C22.0357 28.8439 21.9999 29.1714 22 29.5C22 31.9853 24.0147 34 26.5 34C28.9853 34 31 31.9853 31 29.5C31 27.0147 28.9853 25 26.5 25Z"
-                                fill="#D1D1D1"
-                              />
-                            </svg>
-                            <p>Share</p>
-                          </div>
-                          <div className="event-card-footer-container__price">
-                            <h4>{data.eventTicketPrice}</h4>
-                          </div>
-                        </div>
-                        <div className="event-card-footer-container__btn">
-                          <button
-                            onClick={e =>
-                              eventButtonHandler(
-                                e,
-                                data.eventURL,
-                                data.eventTitle
-                              )
-                            }
-                          >
-                            Buy Tickets
-                          </button>
-                          {/* <a href={data.eventURL}>
+                              Buy Tickets
+                            </button>
+                            {/* <a href={data.eventURL}>
                             <button onClick={}>Buy Tickets</button>
                           </a> */}
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  </article>
-                ))}
+                    </article>
+                  ))}
+                </div>
               </div>
             </div>
           </div>
